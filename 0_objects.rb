0df480adf8445307a949c84729f7be277fb403b3
Sharad Shinde <shindesharad71@gmail.com>
2021-09-04 10:09:57 +0530

    Updated

--- 0_objects.rb
+++ 0_objects.rb
@@ -5,3 +5,10 @@ puts self
 # Talking to objects
 num = 2
 puts num.even?
+
+# Looking up for methods
+puts num.methods
+
+# Invoking methods with arguments
+arr = ['apple', 'oranges', 'potato', 'salad']
+puts arr.index('potato')
