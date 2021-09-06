7360aa0922ace5edf370d2b18b7a671c51d5d369
Sharad Shinde <shindesharad71@gmail.com>
2021-09-06 10:00:43 +0530

    Added: Advance String Operations

--- 1_advance_string_operations.rb
+++ 1_advance_string_operations.rb
@@ -2,3 +2,18 @@ str = 'Fear is the path to the dark side'
 
 # String split
 puts str.split()
+
+# Concatenating Strings
+a = "Ruby"
+b = "Monk"
+
+puts a + b
+puts a.concat(b)
+
+# Replacing a substring
+puts "I should look into your problem when I get time".sub('I','We') # first occurrence
+puts "I should look into your problem when I get time".gsub('I','We') # all occurrences
+
+
+# Find a substring using RegEx
+puts 'RubyMonk Is Pretty Brilliant'.match(/ ./)
