3cf41289c0eb0909343a6e7b405defa7b88d80eb
Sharad Shinde <shindesharad71@gmail.com>
2021-09-07 10:32:10 +0530

    Added: conditions

--- 3_conditions.rb
+++ 3_conditions.rb
@@ -7,3 +7,10 @@ def check_sign(num)
 end
 
 check_sign(5)
+
+# The ternary operator
+def check_number(num)
+    return num > 0 ? "#{num} is positive" : "#{num} is negative"
+end
+
+puts check_number(2)
