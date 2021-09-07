d48413cb79fc17a78810090655a7ef561ff0705d
Sharad Shinde <shindesharad71@gmail.com>
2021-09-07 10:26:32 +0530

    Added: Oprators

--- 2_expressions.rb
+++ 2_expressions.rb
@@ -1,3 +1,11 @@
 # Equal
 name = "Bob"
+age = 22
+
 puts name == "Bob"
+
+# Combining different operators and expressions
+puts age >= 23 && name == 'Bob'
+
+# Negating expressions
+puts !(name == 'Bob')
