bce8a2423ddc246d97a4e6be9849b89297a9c400
Sharad Shinde <shindesharad71@gmail.com>
2021-09-08 09:55:42 +0530

    Added: Array

---
+++ 5_arrays.rb
@@ -0,0 +1,15 @@
+# Create array
+arr = []
+arr1 = Array.new
+
+# Array with multiple elements with different data types
+arr = [1, 'two', 3, 4.0]
+
+# Read elements from array
+puts arr[1]
+
+# Insert elements into array
+arr << 5
+
+# Print array
+puts arr
