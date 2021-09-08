17edd14bcf2e4a1452a898d819f70721560d8a30
Sharad Shinde <shindesharad71@gmail.com>
2021-09-08 09:43:50 +0530

    Added: Loops

---
+++ 4_loops.rb
@@ -0,0 +1,16 @@
+# Do while loop
+loop do
+    monk.meditate
+    break if monk.nirvana?
+end
+
+# Run block N times
+5.times do
+    # Perform the action here
+end
+
+def ring(bell, n)
+    n.times do
+        bell.ring
+    end
+end      
