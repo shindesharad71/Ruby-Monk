202130ad005770931ec8b2ef593429e137a50030
Sharad Shinde <shindesharad71@gmail.com>
2021-09-10 10:24:17 +0530

    Added: Hash

---
+++ 7_hash.rb
@@ -0,0 +1,12 @@
+# Creating Hash
+student_ages = { 
+    "Jack" => 20,
+    "Jill" => 30,
+    "Bob" => 25
+}
+
+# Fetch values from a Hash
+puts student_ages["Jack"]
+
+# Modifying Hash
+student_ages["Jill"] = 21
