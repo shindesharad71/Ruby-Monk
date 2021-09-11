db1cde16c36da24d9376b818c42f68f3d1729221
Sharad Shinde <shindesharad71@gmail.com>
2021-09-11 10:45:18 +0530

    Added: class

--- 8_classes.rb
+++ 8_classes.rb
@@ -6,3 +6,25 @@ puts [].class
 # Validate class
 puts 1.is_a?(Integer)
 puts 1.is_a?(String)
+
+# Create new instance iof class
+Object.new
+
+# Creating Own Classes
+class Rectangle
+    # Code related to class
+
+    # Constructor
+    def initialize(length, breadth)
+        @length = length
+        @breadth = breadth
+    end
+
+    def perimeter
+        2 * (@length + @breadth)
+    end
+
+    def area
+        @length * @breadth
+    end
+end
