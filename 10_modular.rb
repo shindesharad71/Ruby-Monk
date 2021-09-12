7721cbf5e1f23173335deb2bbc4d22113c819e24
Sharad Shinde <shindesharad71@gmail.com>
2021-09-12 12:13:57 +0530

    Added: Lookup Operator

--- 10_modular.rb
+++ 10_modular.rb
@@ -30,3 +30,47 @@ puts Dojo.new.push_ups
 puts WarmUp.class      # Module
 puts Class.superclass   # Module
 puts Module.superclass  # Object
+
+# Module as Namespace
+module Perimeter
+    class Array
+        def initialize
+            @size = 10
+        end
+    end
+end
+
+our_array = Perimeter::Array.new
+ruby_array = Array.new
+
+# Lookup Operator
+module Dojo
+    A = 4
+    module Kata
+        B = 8
+      module Roulette
+        class ScopeIn
+          def push
+            15
+          end
+        end
+      end
+    end
+  end
+  
+  A = 16
+  B = 23
+  C = 42
+  
+  puts "A - #{A}"
+  puts "Dojo::A - #{Dojo::A}"
+  
+  puts
+  
+  puts "B - #{B}"
+  puts "Dojo::Kata::B - #{Dojo::Kata::B}"
+  
+  puts
+  
+  puts "C - #{C}"
+  puts "Dojo::Kata::Roulette::ScopeIn.new.push - #{Dojo::Kata::Roulette::ScopeIn.new.push}"
