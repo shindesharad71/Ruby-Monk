761d1b4d2bc40ba17652372e02fe28c1db794390
Sharad Shinde <shindesharad71@gmail.com>
2021-09-12 12:04:53 +0530

    Added: Modular

--- 10_modular.rb
+++ 10_modular.rb
@@ -1,6 +1,27 @@
 # Defining Module
-module warmup
+module WarmUp
     def push_ups
         "Phew, I need a break"
     end
 end
+
+# Including and Using A Module
+class Gym
+    include WarmUp
+
+    def peacher_curls
+        "I am building my biceps"
+    end
+end
+
+class Dojo
+    include WarmUp
+
+    def tai_kyo_kyu
+        "Look at my stance!"
+    end
+end
+
+# Invoking it
+puts Gym.new.push_ups
+puts Dojo.new.push_ups
