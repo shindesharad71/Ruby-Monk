f6149bb69cf71e6b35c09621d69c577ef85c98cc
Sharad Shinde <shindesharad71@gmail.com>
2021-09-19 11:23:17 +0530

    Added: Inheritance

--- 3_inheritance.rb
+++ 3_inheritance.rb
@@ -16,3 +16,21 @@ puts (
 # The Class Inheritance Example
 class MyArray < Array
 end
+
+class Rectangle
+    def initialize(length, breadth)
+      @length = length
+      @breadth = breadth
+    end
+
+    def perimeter
+      2 * (@length + @breadth)
+    end
+end
+  
+# Create a class Square here
+class Square < Rectangle
+    def initialize(side)
+      @length = @breadth = side
+    end
+end
