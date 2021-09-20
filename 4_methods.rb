fd3e2a48a4d93071df0e82aed38c029223056bee
Sharad Shinde <shindesharad71@gmail.com>
2021-09-20 10:00:02 +0530

    Added: Instance Variables

--- 4_methods.rb
+++ 4_methods.rb
@@ -39,3 +39,18 @@ end
 
 Item.new("tv",1).show
 Item.new("fridge",1).show
+
+# Getters and Setters
+class Item
+    def initialize(item_name, quantity)
+        @item_name = item_name
+        @quantity = quantity
+    end
+    
+    def item_name
+        @item_name
+    end
+end
+
+item = Item.new("tv",1)
+puts item.item_name
