ec0422f6152cfb12152d13f8731757bef269322c
Sharad Shinde <shindesharad71@gmail.com>
2021-09-20 09:58:25 +0530

    Added: Instance Variables

--- 4_methods.rb
+++ 4_methods.rb
@@ -23,3 +23,19 @@ class Rectangle
     def area
     end
 end
+
+# Instance Variables
+class Item
+    def initialize(item_name, quantity)
+        @item_name = item_name
+        @quantity = quantity
+    end
+    
+    def show
+        puts @item_name
+        puts @quantity
+    end
+end
+
+Item.new("tv",1).show
+Item.new("fridge",1).show
