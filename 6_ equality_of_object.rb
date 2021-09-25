6a03c6a0166da7ad03e9c5e214f472c5209f635f
Sharad Shinde <shindesharad71@gmail.com>
2021-09-25 10:15:48 +0530

    Added: Object Equality

---
+++ 6_ equality_of_object.rb
@@ -0,0 +1,30 @@
+puts [1,2] == [1,2]
+puts [1,2] == [1,3]
+
+puts "a" == "xyz"
+
+class Item
+    def initialize(item_name, qty)
+        @item_name = item_name
+        @qty = qty
+    end
+end
+
+p Item.new("abcd",1)  == Item.new("abcd",1)
+
+class Item
+    def initialize(item_name, qty)
+        @item_name = item_name
+        @qty = qty
+    end
+    def to_s
+        "Item (#{@item_name}, #{@qty})"
+    end
+    def ==(other_item)
+        puts "Method == called on #{self} with parameter #{other_item}"
+        false
+    end
+end
+
+# This line should invoke the `==` method defined in the Item class.
+puts Item.new("abcd",1) == Item.new("abcd", 1)
