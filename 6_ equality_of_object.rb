2c98b3a104c6cc28d5c3f15064ccc54ac9954f1c
Sharad Shinde <shindesharad71@gmail.com>
2021-09-25 11:12:04 +0530

    Added: Class Methods

--- 6_ equality_of_object.rb
+++ 6_ equality_of_object.rb
@@ -28,3 +28,21 @@ end
 
 # This line should invoke the `==` method defined in the Item class.
 puts Item.new("abcd",1) == Item.new("abcd", 1)
+
+class Item
+    attr_reader :item_name, :qty
+    
+    def initialize(item_name, qty)
+        @item_name = item_name
+        @qty = qty
+    end
+    def to_s
+        "Item (#{@item_name}, #{@qty})"
+    end
+    def ==(other_item)
+      # your code here
+    end
+end
+
+p Item.new("abcd",1)  == Item.new("abcd",1)
+p Item.new("abcd",2)  == Item.new("abcd",1)
