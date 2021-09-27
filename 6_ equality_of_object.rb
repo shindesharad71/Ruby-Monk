37d642d03ee54b020e1af4e7d6b6e9ab28bea630
Sharad Shinde <shindesharad71@gmail.com>
2021-09-27 09:40:55 +0530

    Added: Uniq Check

--- 6_ equality_of_object.rb
+++ 6_ equality_of_object.rb
@@ -43,3 +43,20 @@ end
 
 p Item.new("abcd",1)  == Item.new("abcd",1)
 p Item.new("abcd",2)  == Item.new("abcd",1)
+
+class Item
+    attr_reader :item_name, :qty
+    def initialize(item_name, qty)
+        @item_name = item_name
+        @qty = qty
+    end
+    def to_s
+        "Item (#{@item_name}, #{@qty})"
+    end
+    def ==(other_item)
+        self.item_name == other_item.item_name && self.qty == other_item.qty
+    end
+end
+
+items = [Item.new("abcd", 1), Item.new("abcd", 1), Item.new("abcd", 1)]
+p items.uniq
