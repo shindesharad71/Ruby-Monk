7279f76726830bcc49cf1c124ef1ee4c71dfd226
Sharad Shinde <shindesharad71@gmail.com>
2021-09-28 09:37:23 +0530

    Added: Displaying Objects

--- 7_displaying_objects.rb
+++ 7_displaying_objects.rb
@@ -7,3 +7,15 @@ end
 puts Item.new
 puts Item.new.to_s
 p Item.new
+
+class Item
+  def initialize(item_name, qty)
+    @item_name = item_name
+    @qty = qty
+  end
+end
+
+item = Item.new("a", 1)
+
+puts item
+p item
