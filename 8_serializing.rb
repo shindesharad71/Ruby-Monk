a6246b16b0925ec1e22022009c0d4f4f10ce70e7
Sharad Shinde <shindesharad71@gmail.com>
2021-09-29 09:42:11 +0530

    Added: Seializing

--- 8_serializing.rb
+++ 8_serializing.rb
@@ -62,3 +62,45 @@ shrek = Ogre.new(62, 12)
 fiona = Ogre.new(66, 37)
 ogres = [shrek, fiona]
 puts "We can even serialize arrays! They're just another object. #{ogres}"
+
+class Ogre
+  attr_accessor :strength, :speed, :smell
+
+  def initialize(strength, speed, smell)
+    @strength = strength
+    @speed = speed
+    @smell = smell
+  end
+end
+
+class Dragon
+  attr_accessor :strength, :speed, :color
+
+  def initialize(strength, speed, color)
+    @strength = strength
+    @speed = speed
+    @color = color
+  end
+end
+
+class Fairy
+  attr_accessor :strength, :speed, :intelligence
+
+  def initialize(intelligence)
+    @strength = 1
+    @speed = 42
+    @intelligence = intelligence
+  end
+end
+
+def save_game(characters)
+  yaml = YAML::dump(characters)
+  game_file = GameFile.new("/my_game/saved.yaml")
+  game_file.write(yaml)
+end
+
+def load_game
+  game_file = GameFile.new("/my_game/saved.yaml")
+  yaml = game_file.read
+  YAML::load(yaml)
+end
