07926247614d49fffad5d7fbd120f91957fa09be
Sharad Shinde <shindesharad71@gmail.com>
2021-10-02 10:11:35 +0530

    Added: Using The API

--- 10_using_the_api.rb
+++ 10_using_the_api.rb
@@ -1,1 +1,8 @@
 (Array.instance_methods - Object.methods).count
+
+puts [4, 8, 15, 16, 23, 42].count
+puts [4, 8, 15, 16, 23, 42].size
+puts [4, 8, 15, 16, 23, 42].length
+
+puts [42, 8, 15, 16, 23, 42].count(42)
+puts ["Jacob", "Alexandra", "Mikhail", "Karl", "Dogen", "Jacob"].count("Jacob")
