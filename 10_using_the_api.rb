ff63f3aacd765b8633d4def895b2818d8bf24730
Sharad Shinde <shindesharad71@gmail.com>
2021-10-03 12:17:30 +0530

    Added: Using The API

--- 10_using_the_api.rb
+++ 10_using_the_api.rb
@@ -14,3 +14,10 @@ puts [4, 8, 15, 16, 23, 42].index { |e| e % 2 == 0 }
 
 p [4, 8, 15, 16, 23, 42].flatten
 p [4, [8], [15], [16, [23, 42]]].flatten
+
+p [nil, 4, nil, 8, 15, 16, nil, 23, 42, nil].compact
+
+def zen(array)
+  converted = array.compact.flatten
+  converted.index(42) == 5 ? converted.count : nil
+end
