d522450222c45905c24a80a7e4615e5edf728847
Sharad Shinde <shindesharad71@gmail.com>
2021-10-04 10:19:09 +0530

    Added: Stacks and Queues

---
+++ 11_stacks_and_queues.rb
@@ -0,0 +1,18 @@
+class Stack
+  def initialize
+    @store = Array.new
+  end
+
+  def pop
+    @store.pop
+  end
+
+  def push(element)
+    @store.push(element)
+    self
+  end
+
+  def size
+    @store.size
+  end
+end
