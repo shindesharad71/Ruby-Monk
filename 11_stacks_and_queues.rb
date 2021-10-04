36015d6da0b8c8d5e0962aeb8f9a6243465e0a86
Sharad Shinde <shindesharad71@gmail.com>
2021-10-04 10:21:00 +0530

    Added: Queue Method

--- 11_stacks_and_queues.rb
+++ 11_stacks_and_queues.rb
@@ -63,3 +63,67 @@ class Stack
     @top == -1
   end
 end
+
+class Queue
+  def initialize
+    @store = Array.new
+  end
+
+  def dequeue
+    @store.pop
+  end
+
+  def enqueue(element)
+    @store.unshift(element)
+    self
+  end
+
+  def size
+    @store.size
+  end
+end
+
+class Queue
+  def initialize(size)
+    @size = size
+    @store = Array.new(@size)
+    @head, @tail = -1, 0
+  end
+
+  def dequeue
+    if empty?
+      nil
+    else
+      @tail = @tail.succ
+
+      dequeued = @store[@head]
+      @store.unshift(nil)
+      @store.pop
+      dequeued
+    end
+  end
+
+  def enqueue(element)
+    if full? or element.nil?
+      nil
+    else
+      @tail = @tail.pred
+      @store[@tail] = element
+      self
+    end
+  end
+
+  def size
+    @size
+  end
+
+  private
+
+  def empty?
+    @head == -1 and @tail == 0
+  end
+
+  def full?
+    @tail.abs == (@size)
+  end
+end
