9aa543559c5135493ee735eee5b3b4688ed44842
Sharad Shinde <shindesharad71@gmail.com>
2021-10-05 08:34:00 +0530

    Added: Disjoint Array

--- 12_disjoint_arrays.rb
+++ 12_disjoint_arrays.rb
@@ -1,3 +1,32 @@
 ["xkcd", 9, Object.new, ["a", "clean", "array"], { :a => "hash" }].each do |e|
   puts e.class
 end
+
+class WaiterRobot
+  def initialize(chef, tables)
+    @chef = chef
+    @tables = tables
+    @name = "Tyler Durden"
+  end
+
+  def name
+    @name
+  end
+
+  def place_order(table_number, sandwich, drink)
+    order = { :table => table_number, :sandwich => sandwich, :drink => drink }
+    @chef.new_order(self, order)
+  end
+
+  def serve(order)
+    # digest the chef's sloppy order instructions:
+    # 1) find the table number you need to serve
+    o = Hash[*order]
+    table_number = o[:table]
+    table = @tables[table_number]
+
+    # 2) call Table#serve_sandwich and Table#serve_drink
+    table.serve_sandwich(o[:sandwich])
+    table.serve_drink(o[:drink])
+  end
+end
