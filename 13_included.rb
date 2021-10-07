5045b131ad3e1562717e6b634b39205435cf04a6
Sharad Shinde <shindesharad71@gmail.com>
2021-10-07 09:51:49 +0530

    Added: Included

--- 13_included.rb
+++ 13_included.rb
@@ -32,3 +32,15 @@ end
 bar = Bar.new
 bar.extend Foo
 bar.module_method
+
+module Foo
+  def method_in_module
+    "The method defined in the module invoked"
+  end
+end
+
+class Bar
+  def initialize
+    self.extend Foo
+  end
+end
