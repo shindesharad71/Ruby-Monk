643256166bf959ccbe6aa358cbccd2c117d104b7
Sharad Shinde <shindesharad71@gmail.com>
2021-10-07 09:51:30 +0530

    Added: Included

--- 13_included.rb
+++ 13_included.rb
@@ -19,3 +19,16 @@ class Bar
 end
 
 Bar.module_level_method
+
+module Foo
+  def module_method
+    puts "Module Method invoked"
+  end
+end
+
+class Bar
+end
+
+bar = Bar.new
+bar.extend Foo
+bar.module_method
