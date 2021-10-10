e068a277cd6270b8d10eb1f1626a40e6a1a9706f
Sharad Shinde <shindesharad71@gmail.com>
2021-10-10 09:11:15 +0530

    Added: Handling and Raising

--- 14_wrapping_up_modules.rb
+++ 14_wrapping_up_modules.rb
@@ -30,3 +30,13 @@ end
 
 puts casual_names(["Yehuda Katz", nil, "Why the lucky stiff"])
 puts "I bet you'll never see this."
+
+def start_summer
+  raise Exception.new("overheated!")
+end
+
+begin
+  start_summer
+rescue Exception
+  puts "It's cool in here, boy. For the rest of the summer, we'll live in the refrigerator."
+end
