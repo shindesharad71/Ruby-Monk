ddd5938dc8689fa34dd986fc7824caa6d50735dd
Sharad Shinde <shindesharad71@gmail.com>
2021-10-10 09:10:37 +0530

    Added: Handling and Raising

--- 14_wrapping_up_modules.rb
+++ 14_wrapping_up_modules.rb
@@ -15,3 +15,18 @@ module Weather
 end
 
 puts Weather.will_it_rain_on(Date.today)
+
+def extract_first_name(name)
+  first = name.split(" ").first
+  puts "extracted first name #{first}"
+  first
+end
+
+def casual_names(names)
+  casual = names.map { |name| extract_first_name(name) }
+  puts "captured casual names #{casual}"
+  casual
+end
+
+puts casual_names(["Yehuda Katz", nil, "Why the lucky stiff"])
+puts "I bet you'll never see this."
