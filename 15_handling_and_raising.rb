3d00d39a0350963eed1592c3d250011ae084e136
Sharad Shinde <shindesharad71@gmail.com>
2021-10-11 10:12:40 +0530

    Added: Handling and Raising

--- 15_handling_and_raising.rb
+++ 15_handling_and_raising.rb
@@ -32,3 +32,17 @@ begin
 rescue Exception => e
   puts "Let me tell you about heat! #{e.inspect}"
 end
+
+EXAMPLE_SECRETS = ["het", "keca", "si", nil, "iel"]
+
+def decode(jumble)
+  secret = jumble.split("").rotate(3).join("")
+  announce(secret)
+  secret
+end
+
+def decode_all(secrets)
+  secrets.map { |s| decode(s) }
+rescue
+  puts "whew! safe."
+end
