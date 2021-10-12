37a907f43297d02ed90cebbadc3468e9f76c3e12
Sharad Shinde <shindesharad71@gmail.com>
2021-10-12 10:28:53 +0530

    Added: Ensure

--- 15_handling_and_raising.rb
+++ 15_handling_and_raising.rb
@@ -46,3 +46,19 @@ def decode_all(secrets)
 rescue
   puts "whew! safe."
 end
+
+class UserDataAccess
+  attr_accessor :db
+
+  def initialize
+    @db = Database.new
+  end
+
+  def find_user(name)
+    @db.find("SELECT * FROM USERS WHERE NAME = '%'", name)
+  rescue Exception => e
+    puts "A database error occurred."
+  ensure
+    @db.close
+  end
+end
