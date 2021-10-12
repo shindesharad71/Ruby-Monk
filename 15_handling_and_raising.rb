9633b33d98df38c6ced2ced4b07729767ce7188b
Sharad Shinde <shindesharad71@gmail.com>
2021-10-12 10:29:19 +0530

    Added: Ensure

--- 15_handling_and_raising.rb
+++ 15_handling_and_raising.rb
@@ -62,3 +62,17 @@ class UserDataAccess
     @db.close
   end
 end
+
+def string_slice(*strings)
+  if strings.size > 5
+    raise ArgumentError, "Only 5 strings are allowed"
+  end
+
+  strings.map do |string|
+    if string.size < 3
+      raise IndexError, "Strings need to be at least 3 characters long"
+    end
+
+    string.slice(0..2)
+  end
+end
