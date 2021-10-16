e63bdbb7d8e7ee6f7171434f22409b63b5eb4574
Sharad Shinde <shindesharad71@gmail.com>
2021-10-16 10:46:07 +0530

    Added: Litrals

--- 17_litrals.rb
+++ 17_litrals.rb
@@ -36,3 +36,11 @@ def big_q_string(numerator, denominator)
   %Q[This %Q syntax is the ugliest one.
 #{numerator} out of #{denominator} "dentists" agree.]
 end
+
+def repetitive_array_of_strings
+  ["Wow,", "this", "is", "a", "pretty", "long", "list", "of", "words", "and", "it", "took", "me", "a", "long", "time", "to", "type", "because", "of", "all", "those", "darn", "quote", "characters.", "Geez."]
+end
+
+def array_of_words_literal
+  %w[With this double-u shorthand it wasn't very hard at all to type out this list of words. Heck, I was even able to use double-quotes like "these"!]
+end
