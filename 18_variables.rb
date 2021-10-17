93e1687e982386dd1fcbcb98262002fe236cb679
Sharad Shinde <shindesharad71@gmail.com>
2021-10-17 11:34:01 +0530

    Added: Variables

--- 18_variables.rb
+++ 18_variables.rb
@@ -5,3 +5,35 @@ end
 
 scope_the_scope
 puts "from the outside: #{defined?(on_the_inside).inspect}"
+
+class Shoe
+  def initialize(toes = 1)
+    @toes = toes
+  end
+
+  puts "inside the class: #{defined?(@toes).inspect}"
+
+  def i_can_haz_toes
+    puts "inside the instance: #{defined?(@toes).inspect}"
+  end
+end
+
+class Foot
+  def initialize(toes = 5)
+    @toes = toes
+  end
+
+  puts "inside the class: #{defined?(@toes).inspect}"
+
+  def i_can_haz_toes
+    puts "inside the instance: #{defined?(@toes).inspect}"
+  end
+end
+
+samurai_boot = Shoe.new(2)
+samurai_boot.i_can_haz_toes
+
+left = Foot.new
+left.i_can_haz_toes
+
+puts "in the `main` class: #{defined?(@toes).inspect}"
