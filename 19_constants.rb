c7fc8b511c8d6d5e4b842da2ad0b95ae807ceb36
Sharad Shinde <shindesharad71@gmail.com>
2021-10-18 10:19:14 +0530

    Added: Constants

--- 19_constants.rb
+++ 19_constants.rb
@@ -5,3 +5,19 @@ def rewrite_history
 end
 
 rewrite_history
+
+fence = Module.new do
+def speak
+	"I'm trapped!"
+end
+end
+
+class Sheep
+def speak
+	"Baaaaahhhhh."
+end
+end
+
+dolly = Sheep.new
+dolly.extend(fence)
+puts dolly.speak
