d76457e95579cc0ae24baf1c8d923c2c33491b3e
Sharad Shinde <shindesharad71@gmail.com>
2021-10-21 08:22:04 +0530

    Added: Transform

--- 21_itrate_transform.rb
+++ 21_itrate_transform.rb
@@ -18,3 +18,15 @@ end
   accumulator += iterated
   accumulator
 end
+
+def custom_inject(array, default = nil)
+  accumulator = default || array[0]
+
+  array.each do |element|
+    accumulator = accumulator + element
+  end
+
+  accumulator
+end
+
+p custom_inject([4, 8, 15, 16, 23, 42], 0)
