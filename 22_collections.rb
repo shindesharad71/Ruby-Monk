d7f4a51a5a4d7f918b343abee45a51b9a71a3b0c
Sharad Shinde <shindesharad71@gmail.com>
2021-10-22 09:51:08 +0530

    Added: Collections

--- 22_collections.rb
+++ 22_collections.rb
@@ -1,3 +1,16 @@
 [1, 2, 3].each do |number|
   puts number
 end
+
+class FibonacciNumbers
+  NUMBERS = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
+  def each
+    NUMBERS.each { |number| yield number }
+  end
+end
+
+f = FibonacciNumbers.new
+f.each do |fibonacci_number|
+  puts "A Fibonacci number multiplied by 10: #{fibonacci_number * 10}"
+end
