9c2f78a695c04f3317e8c2dfc19108e7ddc04ca8
Sharad Shinde <shindesharad71@gmail.com>
2021-10-25 09:50:20 +0530

    Added: Logging

--- 24_logging.rb
+++ 24_logging.rb
@@ -17,3 +17,18 @@ require "logger"
 logger = Logger.new($stdout)
 logger.warn("This is a warning")
 logger.info("This is an info")
+
+require "logger"
+logger = Logger.new(STDOUT)
+
+logger.level = Logger::UNKNOWN
+logger.debug("(UNKNOWN) This a debug message")
+logger.unknown("(UNKNOWN) Something unknown. Oh, mystery and suspense!")
+logger.error("(UNKNOWN) Error! Run! Panic!")
+logger.warn("(UNKNOWN) This is a warning.")
+
+logger.level = Logger::INFO
+logger.debug("(INFO) This a debug message")
+logger.unknown("(INFO) Something unknown. Oh, mystery and suspense!")
+logger.error("(INFO) Error! Run! Panic!")
+logger.warn("(INFO) This is a warning.")
