d630df1efb7fa4cac30a7f25fa3f6b77824b3178
Sharad Shinde <shindesharad71@gmail.com>
2021-10-26 09:48:16 +0530

    Added: Benchmarking

--- 25_benchmarking.rb
+++ 25_benchmarking.rb
@@ -2,3 +2,7 @@ begin_time = Time.now
 sleep 0.3
 end_time = Time.now
 puts "Time taken is #{end_time - begin_time}."
+
+require "benchmark"
+
+puts Benchmark.measure { 602214.times { 3.14159 * 6.626068 } }
