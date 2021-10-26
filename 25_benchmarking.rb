2f9f38819cc2a0886c4459950c1f81410e5e1d24
Sharad Shinde <shindesharad71@gmail.com>
2021-10-26 09:48:35 +0530

    Added: Benchmarking

--- 25_benchmarking.rb
+++ 25_benchmarking.rb
@@ -6,3 +6,18 @@ puts "Time taken is #{end_time - begin_time}."
 require "benchmark"
 
 puts Benchmark.measure { 602214.times { 3.14159 * 6.626068 } }
+
+require "benchmark"
+
+n = 4000
+Benchmark.bm do |benchmark|
+  benchmark.report do
+    a = []; n.times { a = a + [n] }
+  end
+  benchmark.report do
+    a = []; n.times { a << n }
+  end
+  benchmark.report do
+    a = [1..n].map { |number| number }
+  end
+end
