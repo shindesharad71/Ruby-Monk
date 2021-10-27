c26b6ff657bdacfd65cf5347b1b55542a3ad8db0
Sharad Shinde <shindesharad71@gmail.com>
2021-10-27 10:01:41 +0530

    Added: Objects

--- 26_object.rb
+++ 26_object.rb
@@ -22,3 +22,43 @@ p Foo.class
 p String.class
 p Fixnum.class
 p Array.class
+
+class Food
+end
+
+class Soup < Food
+end
+
+class IceCream < Food
+end
+
+class ChineseGreenBeans < Food
+end
+
+class DeliveryTray
+  FOOD_DISH_MAPPING = {
+    Soup => "soup bowl",
+    IceCream => "ice cream bowl",
+    ChineseGreenBeans => "serving plate",
+  }
+
+  def initialize
+    @dishes_needed = {}
+  end
+
+  def add(food)
+    dish = FOOD_DISH_MAPPING[food.class]
+    @dishes_needed[dish] = (@dishes_needed[dish] || 0) + 1
+  end
+
+  def dishes_needed
+    return "None." if @dishes_needed.empty?
+
+    @dishes_needed.map { |dish_name, count| "#{count} #{dish_name}" }.join(", ")
+  end
+end
+
+d = DeliveryTray.new
+d.add Soup.new; d.add Soup.new
+d.add IceCream.new
+puts d.dishes_needed # should be "2 soup bowl, 1 ice cream bowl"
