e3ec422bc026e9668195e1dbdd585c8e6112967d
Sharad Shinde <shindesharad71@gmail.com>
2021-10-28 09:50:54 +0530

    Added: Behaviour

--- 27_behaviour.rb
+++ 27_behaviour.rb
@@ -4,3 +4,9 @@ end
 foo = Foo.new
 p foo.send(:object_id)  # this is same as foo.object_id
 p foo.send(:class)      # this is same as foo.class
+
+class Foo
+end
+
+p Foo.is_a?(Object)
+p Foo.new.is_a?(Object)
