0f955841e1f4405d83ea6f70436c21f336a0eb29
Sharad Shinde <shindesharad71@gmail.com>
2021-10-29 09:46:47 +0530

    Added: Singleton

--- 28_singleton.rb
+++ 28_singleton.rb
@@ -25,3 +25,29 @@ end
 foo.shout
 
 p Foo.new.respond_to?(:shout)
+
+class Object
+  def metaclass
+    class << self
+      self
+    end
+  end
+end
+
+foo = "I'm a string object"
+
+# Let us define a singleton method on foo
+def foo.shout
+  puts self.upcase
+end
+
+foo.shout
+
+# Lets look at the class of foo's metaclass.
+p foo.metaclass.class
+
+# The singleton method 'shout' obviously doesn't exist in the actual class.
+p foo.class.instance_methods.include? :shout
+
+# But does it exist in the metaclass?
+p foo.metaclass.instance_methods.include? :shout
