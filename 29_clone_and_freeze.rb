f37854d22cdefcdc64b0201278a5c57a93208aa3
Sharad Shinde <shindesharad71@gmail.com>
2021-10-30 10:11:48 +0530

    Added: Clone and Freeze Objects

--- 29_clone_and_freeze.rb
+++ 29_clone_and_freeze.rb
@@ -11,3 +11,13 @@ b << 4
 
 p a
 p b
+
+foo = "foo"
+foos = [foo]
+foo = foo.upcase		# returns a new string object. foo remains unharmed.
+p foos
+
+bar = "bar"
+bars = [bar]
+bar.upcase!				  # this changes bar itself. the object mutates.
+p bars
