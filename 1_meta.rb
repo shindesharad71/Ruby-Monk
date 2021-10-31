985224ae418b51241f763ec67b216f669195e171
Sharad Shinde <shindesharad71@gmail.com>
2021-10-31 10:12:19 +0530

    Added: Meta

--- 1_meta.rb
+++ 1_meta.rb
@@ -11,3 +11,15 @@ class SetInStone
 end
 
 print SetInStone.new.everything_changes
+
+class SetInStone
+  #empty class
+end
+
+class SetInStone
+  def everything_changes
+    return "Wait, what? You just added a method to me!"
+  end
+end
+
+print SetInStone.new.everything_changes
