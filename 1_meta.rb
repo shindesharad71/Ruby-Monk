1b545dde0c070c7b3157fdb3c5ca8c17c2a0cd16
Sharad Shinde <shindesharad71@gmail.com>
2021-10-31 10:11:27 +0530

    Added: Meta

---
+++ 1_meta.rb
@@ -0,0 +1,7 @@
+class Array
+  def foldl(method)
+    inject { |result, i| result ? result.send(method, i) : i }
+  end
+end
+
+puts [1000.0, 200.0, 50.0].foldl("/") # Feel free to experiment with this
