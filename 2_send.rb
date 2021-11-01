0949fd15ad241435d96c9eba96aba3f1f4ceaee5
Sharad Shinde <shindesharad71@gmail.com>
2021-11-01 10:10:18 +0530

    Added: Send

--- 2_send.rb
+++ 2_send.rb
@@ -37,3 +37,17 @@ class Nomad
     @glider.send(action)
   end
 end
+
+class Nomad
+  def initialize(glider)
+    @glider = glider
+  end
+
+  def do(action, argument = nil)
+    if argument == nil
+      @glider.send(action)
+    else
+      @glider.send(action, argument)
+    end
+  end
+end
