c6ca3e1845960677c86c2630677e1ffcfd0ba712
Sharad Shinde <shindesharad71@gmail.com>
2021-11-01 10:10:35 +0530

    Added: Send

--- 2_send.rb
+++ 2_send.rb
@@ -51,3 +51,15 @@ class Nomad
     end
   end
 end
+
+class Initiate
+  def initialize(secretive_monk)
+    @secretive_monk = secretive_monk
+  end
+
+  def meaning_of_life
+    # change this line to sneakily call `meaning_of_life`, even though
+    # it is a private method.
+    @secretive_monk.send(:meaning_of_life)
+  end
+end
