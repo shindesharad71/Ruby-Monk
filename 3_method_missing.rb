3af4d2640abcf2156171496282120c48bd4c168e
Sharad Shinde <shindesharad71@gmail.com>
2021-11-02 10:12:56 +0530

    Added: Method Missing

--- 3_method_missing.rb
+++ 3_method_missing.rb
@@ -2,3 +2,9 @@ class Spy
 end
 
 Spy.new.hello # NoMethodError: undefined method `hello'
+
+class Spy
+  def method_missing(sym, *args, &block)
+    puts "#{sym} was called with #{args} and #{block}"
+  end
+end
